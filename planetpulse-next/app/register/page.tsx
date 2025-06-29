b95ef191ae1fa91483021a010963a7e36309d9a6
Bernardo <tradestuga@gmail.com>
2025-06-29 23:21:59 +0100

    updates try

--- planetpulse-next/app/register/page.tsx
+++ planetpulse-next/app/register/page.tsx
@@ -54,7 +54,7 @@ export default function RegisterPage() {
           type="submit"
           className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700"
         >
-          Registerr
+          Registerrrr
         </button>
       </form>
     </div>
